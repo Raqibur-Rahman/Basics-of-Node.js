63149154a56e619371ea6afd92243324121bfd2e
Raqibur-Rahman <raqibur.edu@gmail.com>
2022-05-03 20:21:09 +0600

    Again starting from scratch

--- firstNodeJsServerByhttpModule.js
+++ firstNodeJsServerByhttpModule.js
@@ -1,7 +1,8 @@
 var http = require('http');
 
-var server = http.createServer(function (request,respond) {
-    respond.end("Assalaamu alaikum\nWelcome to new Journey!")
+var server = http.createServer(function (request, respond){
+      respond.end("Assalaamu alaikum!")
 });
-server.listen(5050); // open localhost:5050 to check whether it is running in your server or not!
-console.log("Alhamdulillah, Server is running!"); // this will be printed in the console for successful action 
+
+server.listen(5050);
+console.log("Server is Running")
